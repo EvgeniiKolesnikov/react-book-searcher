85ba6af31c7cf599db5f9d1c38f9fa0adf006da9
Evgenii Kolesnikov <jekow@yandex.ru>
2021-10-15 20:26:30 +0300

    New design of BookCard component

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -12,28 +12,28 @@ export const BookCard: React.FC<IBookCard> = ({
 }) => {
   return (
     <div className={styles['book-card']} id={id}>
-      <div className={styles['book-card__container']}>
-        <img
-          className={styles['book-card__img']}
-          alt='book cover'
-          src={
-            cover_i
-              ? `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg?default=false`
-              : isbn
-              ? `https://covers.openlibrary.org/b/isbn/${isbn[0]}-M.jpg?default=false`
-              : skin
-          }
-          onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-            e.currentTarget.onerror = null;
-            e.currentTarget.src = skin;
-          }}
-        />
-        <div className={styles['book-card__header']}>
-          <span className={styles['book-card__title']}>{title}</span>
-          {author_name && (
-            <div className={styles['book-card__author']}>{author_name.join(", ")}</div>
-          )}
-        </div>
+      <img
+        className={styles['book-card__img']}
+        alt='book cover'
+        src={
+          cover_i
+            ? `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg?default=false`
+            : isbn
+            ? `https://covers.openlibrary.org/b/isbn/${isbn[0]}-M.jpg?default=false`
+            : skin
+        }
+        onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+          e.currentTarget.onerror = null;
+          e.currentTarget.src = skin;
+        }}
+      />
+      <div className={styles['book-card__description']}>
+        <div className={styles['book-card__title']}>{title}</div>
+        {author_name && (
+          <div className={styles['book-card__author']}>
+            {author_name.join(', ')}
+          </div>
+        )}
       </div>
     </div>
   );
