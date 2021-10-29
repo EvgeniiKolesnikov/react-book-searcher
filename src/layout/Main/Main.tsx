4a69981389f6715084908545a173ba7cab4b0e74
Evgenii Kolesnikov <jekow@yandex.ru>
2021-10-29 23:05:27 +0300

    Modify Main

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,33 +1,9 @@
-import { useState } from 'react';
-import { BooksList, Preloader, SearchBar } from '../../components';
-import { IBookCard, IBooksData } from '../../interfaces/interfaces';
-import { OpenLibraryService } from '../../services/OpenLibraryService';
 import './Main.scss';
 
-export const Main: React.FC = ({children}) => {
-  // const [booksList, setBooksList] = useState<IBookCard[]>([]);
-  // const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const handleSearch = async (searchValue: string) => {
-    // setBooksList([]);
-    // setIsLoading(true);
-    // const data:IBooksData = await OpenLibraryService.searchBooks(searchValue);
-    // console.log(data);
-    // data.docs.forEach((item: IBookCard) => {
-    //   item.id = item.key.split("/").pop();
-    // });
-    // setBooksList(data.docs);
-    // setIsLoading(false);
-    // console.log(data.docs);
-  };
+export const Main: React.FC = ({ children }) => {
   return (
     <main className='main'>
-      <div className='main__container'>
-        {/* <SearchBar onSearch={handleSearch} /> */}
-        {children}
-        {/* {booksList.length > 0 && <BooksList booksList={booksList} />} */}
-      </div> 
-      
+      <div className='main__container'>{children}</div>
     </main>
   );
 };
