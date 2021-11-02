db2755e7aba46186d13d4164bba0283316368a90
Evgenii Kolesnikov <jekow@yandex.ru>
2021-11-02 23:51:47 +0300

    Modify dataReducer

--- src/store/reducers/dataReducer.ts
+++ src/store/reducers/dataReducer.ts
@@ -1,4 +1,5 @@
 import { DataAction, DataActionTypes, IDataState } from '../types/data';
+import _ from 'lodash';
 
 const initialState: IDataState = {
   data: null,
@@ -12,11 +13,23 @@ export const DataReducer = (
   state = initialState,
   action: DataAction
 ): IDataState => {
+  const customizer = (obj1: IDataState, obj2: IDataState) => {
+    if (_.isArray(obj1)) return _.concat(obj1, obj2);
+  };
+
   switch (action.type) {
     case DataActionTypes.FETCH_DATA_LOADING:
-      return { ...state, loading: true, error: null, data: null };
+      return { ...state, loading: true, error: null };
     case DataActionTypes.FETCH_DATA_SUCESS:
-      return { ...state, loading: false, error: null, data: action.payload };
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        data:
+          state.page === 1
+            ? action.payload
+            : _.mergeWith(state.data, action.payload, customizer),
+      };
     case DataActionTypes.FETCH_DATA_ERROR:
       return { ...state, loading: false, error: action.payload, data: null };
     case DataActionTypes.SET_DATA_QUERY:
