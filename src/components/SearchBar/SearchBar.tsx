9a77616bffbec86b2669f721a335323431f54447
Evgenii Kolesnikov <jekow@yandex.ru>
2022-02-07 15:15:00 +0300

    Sample code using inputRef+timerRef instead useEffect+useState (temp). Fewer re-rendered

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,54 +1,67 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useEffect, useState } from 'react';
 import { useActions } from '../../hooks/useActions';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import './SearchBar.scss';
 
 export const SearchBar: React.FC = () => {
-  const [value, setValue] = useState('');
+  // const [value, setValue] = useState('');
   const { fetchData, setDataQuery, setDataPage } = useActions();
-  const { query, page, data, loading } = useTypedSelector(
-    (state) => state.data
-  );
-  // console.log('render BAR', value, query, page, loading);
+  const { query, page, loading } = useTypedSelector((state) => state.data);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  console.log(`render BAR , p=${page}, l=${loading}, q=${query}, v=${inputRef?.current?.value}`);
 
   // load/add new page
   useEffect(() => {
     // console.log('ef(query, page)', loading);
     if (page >= 1) fetchData(query, page);
-    console.log(`query = ${query}, page = ${page}, load new page, ${loading}`);
-    // console.log(data);
+    console.log(`load page p=${page}, l=${loading}, q=${query}, v=${inputRef?.current?.value}`);
   }, [query, page]);
 
   // debouce. Autoload the first page after 1 sec.
-  useEffect(() => {
-    // console.log('ef(timer)', loading);
-    const timer = setTimeout(() => {
-      // console.log('ef(updateQuery timer on)', loading);
-      updateQuery();
-      // console.log('ef(updateQuery timer off)', loading);
-    }, 1000);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [value]);
-
-  const onKeyDown = (e: React.KeyboardEvent) => {
-    // console.log('onKeyDown');
-    if (e.key === 'Enter') {
-      // console.log('updateQuery onKeyDown', loading);
-      updateQuery();
-    }
-  };
+  // useEffect(() => {
+  //   console.log('ef(timer)', loading);
+  //   const timer = setTimeout(() => {
+  //     updateQuery();
+  //   }, 1000);
+  //   return () => {
+  //     clearTimeout(timer);
+  //   };
+  // }, [value]);
 
   const updateQuery = () => {
-    // console.log('updateQuery in', loading);
-    !value && document.getElementById('searchInput')?.focus();
-    if (value !== query) {
+    console.log('updateQuery in', loading);
+    console.log('=== updateQuery ===', loading);
+    console.log('v =', inputRef.current?.value);
+    console.log('q = ', query);
+    if (inputRef?.current?.value !== query) {
+      console.log('=== updateQuery work ===', loading);
       setDataPage(1);
-      setDataQuery(value);
+      setDataQuery(inputRef.current!.value);
     }
   };
+
+  const handleClickButton = () => {
+    inputRef.current?.focus();
+    updateQuery();
+  };
+
+  const handleClearButton = () => {
+    inputRef.current!.value = ''
+    updateQuery();
+  };
+
+    // debouce function. Autoload the first page after 1 sec.
+  const handleChangeInput = useCallback(() => {
+    console.log('clearTimeout');
+    clearTimeout(timerRef.current as NodeJS.Timeout);
+    timerRef.current = setTimeout(() => {
+      console.log('Timeout(1000)', 'q = ', query, inputRef.current!.value, loading);
+      updateQuery();
+    }, 1000);
+  }, [query]);
 
   return (
     <div className='input'>
@@ -56,25 +69,28 @@ export const SearchBar: React.FC = () => {
         <input
           className='input__control'
           id='searchInput'
+          ref={inputRef}
           placeholder='. . .'
           type='search'
           name='search'
           title='Введите запрос'
-          value={value}
-          onKeyDown={onKeyDown}
-          onChange={(e) => setValue(e.target.value)}
+          // value={value}
+          onKeyDown={(e) => e.key === 'Enter' && updateQuery()}
+          // onChange={(e) => setValue(e.target.value)}
+          onChange={handleChangeInput}
         />
         <div
           className='input__clear'
           id='searchClear'
           unselectable='on'
-          onClick={() => setValue('')}
-          style={value ? { display: 'block' } : { display: 'none' }}
+          // onClick={() => setValue('')}
+          onClick={handleClearButton}
+          style={inputRef.current?.value ? { display: 'block' } : { display: 'none' }}
         />
-        <button
+        <div
           className='input__button'
           id='searchButton'
-          onClick={() => updateQuery()}
+          onClick={handleClickButton}
         />
       </div>
     </div>
